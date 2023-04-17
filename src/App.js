61390961de30d928977bd03521d7e0d35b94ed35
aasmah <35153854+aasmah@users.noreply.github.com>
2023-04-16 23:05:29 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,21 @@ import React from "react";
 import { useFormik } from 'formik';
 function App() {
   // TODO: add a const called formik assigned to useFormik()
-
+const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      alert("Login Successful");
+    },
+    validate: (values) => {
+      let errors = {};
+      if (!values.email) errors.email = "field required";
+      if (!values.password) errors.password = "field required";
+      return errors;
+    },
+  });
   return (
     <div>
       <p>
